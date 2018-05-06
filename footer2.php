5fc4406949fd276668e10321663b71911b966f75
ruwandi <ruwandi1102@gmail.com>
2018-05-06 16:13:02 +0530

    this is header for all page

---
+++ footer2.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/headerfooter.css">
+</head>
+<body>
+	<hr>
+	<div class="text">Online Blood Bank</div>
+
+	</div>
+</body>
+</html>
