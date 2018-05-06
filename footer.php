3a6852072a77f124aaf4197cad2d9f4220cd5296
ruwandi <ruwandi1102@gmail.com>
2018-05-06 16:19:29 +0530

    This is header file for all page

---
+++ footer.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/headerfooter.css">
+</head>
+<body>
+	<div class="top_bar">
+		<div class="links">
+			<ul>
+  				<li><a class="active" href="home.php">Home</a></li>
+  				<li><a href="service.php">Service</a></li>
+ 				<li><a href="contact.php">Contact</a></li>
+  				<li><a href="about.php">About</a></li>
+  				<li><a href="admin.php">Admin</a></li>
+  				<li style="float:right"><a href="about.asp">Sign in</a></li>
+  				<li style="float:right"><a href="about.asp">Log in</a></li>
+			</ul> 
+		</div>
+
+	</div>
+</body>
+</html>
